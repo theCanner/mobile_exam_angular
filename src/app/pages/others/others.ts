90fc53a1227885c6bf0f0bbcf7a8ce8e0d777e4b
John Acierda <jcanneracierda@gmail.com>
2026-06-22 06:42:29 +0800

    add other screen

--- src/app/pages/others/others.ts
+++ src/app/pages/others/others.ts
@@ -1,9 +1,78 @@
-import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, OnDestroy, OnInit, signal } from '@angular/core';
+import { CustomButton } from '../../components/custom-button/custom-button';
+
+type Brand = {
+  name: string;
+  imageUrl: string;
+  webUrl: string;
+};
 
 @Component({
   selector: 'app-others',
-  imports: [],
+  imports: [CustomButton],
   templateUrl: './others.html',
   styleUrl: './others.css',
 })
-export class Others {}
+export class Others implements OnInit, OnDestroy {
+  brands: Brand[] = [
+    {
+      name: 'Samsung',
+      imageUrl: '/assets/samsung.png',
+      webUrl: 'https://www.samsung.com/',
+    },
+    {
+      name: 'Apple',
+      imageUrl: '/assets/apple.png',
+      webUrl: 'https://www.apple.com/',
+    },
+    {
+      name: 'Windows',
+      imageUrl: '/assets/windows.png',
+      webUrl: 'https://www.microsoft.com/windows/',
+    },
+  ];
+
+  activeIndex = signal(0);
+  private autoplayId?: ReturnType<typeof setInterval>;
+
+  constructor(private location: Location) {}
+
+  ngOnInit() {
+    this.autoplayId = setInterval(() => {
+      this.nextSlide();
+    }, 4000);
+  }
+
+  ngOnDestroy() {
+    if (this.autoplayId) {
+      clearInterval(this.autoplayId);
+    }
+  }
+
+  get activeBrand(): Brand {
+    return this.brands[this.activeIndex()];
+  }
+
+  get carouselTransform(): string {
+    return `translateX(calc(8% - ${this.activeIndex() * 84}%))`;
+  }
+
+  goBack() {
+    this.location.back();
+  }
+
+  nextSlide() {
+    this.activeIndex.update((index) => (index + 1) % this.brands.length);
+  }
+
+  previousSlide() {
+    this.activeIndex.update((index) =>
+      index === 0 ? this.brands.length - 1 : index - 1
+    );
+  }
+
+  visitWebsite() {
+    window.open(this.activeBrand.webUrl, '_blank', 'noopener,noreferrer');
+  }
+}
